551d2c3aba6f2300896576a85e50071655f3ba4f
marlon-erler <viridian035@gmail.com>
2026-04-01 08:46:10 +0000

    restore modal animation on desktop

--- dist/sw.js
+++ dist/sw.js
@@ -1,31 +1,32 @@
 const CACHE_VERSION = "v2";
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event));
+    event.respondWith(handleRequest(event));
 });
 
 async function handleRequest(event) {
-  const response = await getFromCache(event);
-  if (response) {
-    fetchAndCache(event); // Call this but don't await, allowing the cache to update in the background
-    return response;
-  }
-  return fetchAndCache(event);
+    const response = await getFromCache(event);
+    if (response) {
+        fetchAndCache(event); // Call this but don't await, allowing the cache to update in the background
+        return response;
+    }
+    return fetchAndCache(event);
 }
 
 async function getFromCache(event) {
-  const request = event.request;
-  const cache = await caches.open(CACHE_VERSION);
-  const response = await cache.match(request);
-  return response;
+    const request = event.request;
+    const cache = await caches.open(CACHE_VERSION);
+    const response = await cache.match(request);
+    return response;
 }
 
 async function fetchAndCache(event) {
-  const request = event.request;
-  const response = await fetch(request);
-  if (response.status === 200) { // Only cache successful responses
-    const cache = await caches.open(CACHE_VERSION);
-    cache.put(request, response.clone());
-  }
-  return response;
+    const request = event.request;
+    const response = await fetch(request);
+    if (response.status === 200) {
+        // Only cache successful responses
+        const cache = await caches.open(CACHE_VERSION);
+        cache.put(request, response.clone());
+    }
+    return response;
 }
